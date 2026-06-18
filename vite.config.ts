a53af9c9b0a5605a6e7251da5bd3e4de1fe785e3
wanghaiming <cndeepeye@gmail.com>
2026-06-18 21:15:17 +0800

    chore: add Vite config and HTML entry

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    target: 'es2022',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'konva-vendor': ['konva', 'react-konva'],
+          'zip-vendor': ['jszip', 'file-saver'],
+        },
+      },
+    },
+  },
+  server: {
+    port: 5173,
+    strictPort: false,
+    open: false,
+  },
+});
